070b3c03722ea1b04cfc8cbc42e76b34acfb2557
Lars Magnusson <lavima@gmail.com>
2021-10-20 10:30:55 +0200

    Added foreground service

--- Lecture11_Services/After_Java_Services/app/src/main/java/no/hiof/larseknu/playingwithservices/MainActivity.java
+++ Lecture11_Services/After_Java_Services/app/src/main/java/no/hiof/larseknu/playingwithservices/MainActivity.java
@@ -1,6 +1,10 @@
 package no.hiof.larseknu.playingwithservices;
 
+import android.Manifest;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.ResultReceiver;
@@ -10,22 +14,31 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.work.Constraints;
 import androidx.work.Data;
 import androidx.work.OneTimeWorkRequest;
 import androidx.work.WorkManager;
 
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
+import no.hiof.larseknu.playingwithservices.service.MyForegroundService;
 import no.hiof.larseknu.playingwithservices.service.MyIntentService;
 import no.hiof.larseknu.playingwithservices.service.MyJobIntentService;
 import no.hiof.larseknu.playingwithservices.service.MyStartedService;
+import pub.devrel.easypermissions.AfterPermissionGranted;
+import pub.devrel.easypermissions.AppSettingsDialog;
+import pub.devrel.easypermissions.EasyPermissions;
 
 import static no.hiof.larseknu.playingwithservices.MyWorker.WORKER_FILENAME;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements EasyPermissions.PermissionCallbacks {
     private TextView resultTextView;
+    private static final int PERMISSION_ID = 12;
+
+    private String[] neededPermissions = { Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.WRITE_EXTERNAL_STORAGE };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,10 +46,29 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         resultTextView = findViewById(R.id.resultTextView);
+        createNotificationChannel();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        // Forward results to EasyPermissions
+        EasyPermissions.onRequestPermissionsResult(requestCode, permissions, grantResults, this);
     }
 
     public void startStartedService(View view) {
-        startService(new Intent(this, MyStartedService.class));
+        startStartedService_();
+    }
+
+    //@AfterPermissionGranted(PERMISSION_ID)
+    private void startStartedService_() {
+        if (EasyPermissions.hasPermissions(this, neededPermissions)) {
+            startService(new Intent(this, MyStartedService.class));
+        }
+        else {
+            EasyPermissions.requestPermissions(this, "We need permission", PERMISSION_ID, neededPermissions);
+        }
     }
 
     public void stopStartedService(View view) {
@@ -44,21 +76,31 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void scheduleJob(View view) {
-        Constraints constraints = new Constraints.Builder()
-                .setRequiresCharging(true)
-                .build();
+        scheduleJob_();
+    }
 
-        Data fileName = new Data.Builder().putString(WORKER_FILENAME, "MyWorker.txt").build();
+    //@AfterPermissionGranted(PERMISSION_ID)
+    private void scheduleJob_() {
+        if (EasyPermissions.hasPermissions(this, neededPermissions)) {
+            Constraints constraints = new Constraints.Builder()
+                    .setRequiresCharging(true)
+                    .build();
 
-        OneTimeWorkRequest saveFileWorkRequest = new OneTimeWorkRequest.Builder(MyWorker.class)
-                .setInitialDelay(5, TimeUnit.SECONDS)
-                .setInputData(fileName)
-                .setConstraints(constraints)
-                .build();
+            Data fileName = new Data.Builder().putString(WORKER_FILENAME, "MyWorker.txt").build();
 
-        WorkManager.getInstance(this).enqueue(saveFileWorkRequest);
+            OneTimeWorkRequest saveFileWorkRequest = new OneTimeWorkRequest.Builder(MyWorker.class)
+                    .setInitialDelay(5, TimeUnit.SECONDS)
+                    .setInputData(fileName)
+                    .setConstraints(constraints)
+                    .build();
 
-        Log.i("MainActivity", "MyWorker - Called");
+            WorkManager.getInstance(this).enqueue(saveFileWorkRequest);
+
+            Log.i("MainActivity", "MyWorker - Called");
+        }
+        else {
+            EasyPermissions.requestPermissions(this, "We need permission", PERMISSION_ID, neededPermissions);
+        }
     }
 
     @Override
@@ -67,11 +109,49 @@ public class MainActivity extends AppCompatActivity {
         stopService(new Intent(this, MyStartedService.class));
     }
 
+
+    public void startForegroundService(View view) {
+        startForegroundService_();
+    }
+
+    //@AfterPermissionGranted(PERMISSION_ID)
+    private void startForegroundService_() {
+        if (EasyPermissions.hasPermissions(this, neededPermissions)) {
+            Log.i("startForegroundService", "Thread: " + Thread.currentThread().getName());
+            startForegroundService(new Intent(this, MyForegroundService.class));
+        }
+        else {
+            EasyPermissions.requestPermissions(this, "We need permission", PERMISSION_ID, neededPermissions);
+        }
+    }
+
     public void saveAddressIntentService(View view) {
-        // This uses IntentService with ResultReceiver
-        // MyIntentService.startActionRetreiveAndSaveAddress(this, "MyIntentService.txt", new MyResultReceiver(null));
-        // This uses JobIntentService
-        MyJobIntentService.enqueueWork(this, "MyJobIntentService.txt", new MyResultReceiver(null));
+        saveAddressIntentService_();
+    }
+
+    //@AfterPermissionGranted(PERMISSION_ID)
+    private void saveAddressIntentService_() {
+        if (EasyPermissions.hasPermissions(this, neededPermissions)) {
+            // This uses IntentService with ResultReceiver
+            // MyIntentService.startActionRetreiveAndSaveAddress(this, "MyIntentService.txt", new MyResultReceiver(null));
+            // This uses JobIntentService
+            MyJobIntentService.enqueueWork(this, "MyJobIntentService.txt", new MyResultReceiver(null));
+        }
+        else {
+            EasyPermissions.requestPermissions(this, "We need permission", PERMISSION_ID, neededPermissions);
+        }
+    }
+
+    @Override
+    public void onPermissionsGranted(int requestCode, @NonNull List<String> perms) {
+        // Add logic here if you want the operation that was put on hold to run
+    }
+
+    @Override
+    public void onPermissionsDenied(int requestCode, @NonNull List<String> perms) {
+        if (EasyPermissions.somePermissionPermanentlyDenied(this, perms)) {
+            new AppSettingsDialog.Builder(this).build().show();
+        }
     }
 
     private class MyResultReceiver extends ResultReceiver {
@@ -115,5 +195,21 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private void createNotificationChannel() {
+        // Create the NotificationChannel, but only on API 26+ because
+        // the NotificationChannel class is new and not in the support library
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            CharSequence name = "test_channel";
+            String description = "Test Channel";
+            int importance = NotificationManager.IMPORTANCE_HIGH;
+            NotificationChannel channel = new NotificationChannel(name.toString(), name, importance);
+            channel.setDescription(description);
+            // Register the channel with the system; you can't change the importance
+            // or other notification behaviors after this
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
     }
 }
---
+++ Lecture11_Services/After_Java_Services/app/src/main/java/no/hiof/larseknu/playingwithservices/service/MyForegroundService.java
@@ -0,0 +1,97 @@
+package no.hiof.larseknu.playingwithservices.service;
+
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.PendingIntent;
+import android.app.Service;
+import android.content.Intent;
+import android.location.Location;
+import android.os.IBinder;
+import android.util.Log;
+
+import androidx.annotation.Nullable;
+import androidx.core.app.NotificationChannelCompat;
+import androidx.core.app.NotificationCompat;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+import no.hiof.larseknu.playingwithservices.LocationWorker;
+import no.hiof.larseknu.playingwithservices.MainActivity;
+
+public class MyForegroundService extends Service {
+
+    private static final String LOGTAG = MyForegroundService.class.getSimpleName();
+
+    private LocationWorker locationWorker;
+
+    private ExecutorService executorService;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        locationWorker = new LocationWorker(this);
+        executorService = Executors.newFixedThreadPool(3);
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        Log.i(LOGTAG, "onStartCommand Thread: " + Thread.currentThread().getName());
+
+        Intent notificationIntent = new Intent(this, MainActivity.class);
+        PendingIntent pendingIntent =
+                PendingIntent.getActivity(this, 0, notificationIntent, 0);
+
+        Notification notification =
+                new Notification.Builder(getApplicationContext(), "test_channel")
+                        .setContentTitle("Notification Title")
+                        .setContentText("Text")
+                        .setContentIntent(pendingIntent)
+                        .build();
+
+
+        startForeground(123, notification);
+
+        MyForegroundService.ServiceRunnable runnable = new MyForegroundService.ServiceRunnable();
+        executorService.execute(runnable);
+
+        return Service.START_NOT_STICKY;
+    }
+
+    @Override
+    public void onDestroy() {
+        Log.i(LOGTAG, "onDestroy Thread: " + Thread.currentThread().getName());
+        super.onDestroy();
+    }
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    private class ServiceRunnable implements Runnable {
+        @Override
+        public void run() {
+            Log.i(LOGTAG, "MyForegroundService.ServiceRunnable Thread: " + Thread.currentThread().getName());
+            try {
+                Location location = locationWorker.getLocation();
+
+                String address = locationWorker.reverseGeocode(location);
+
+                JSONObject jsonObject = locationWorker.getJSONObjectFromURL("https://www.omdbapi.com/?i=tt3896198&apikey=2f6990a0");
+
+                locationWorker.saveToFile(location, address, jsonObject.getString("Title"), "MyStartedService.txt");
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+            stopForeground(true);
+        }
+    }
+}
